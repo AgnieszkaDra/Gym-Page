064bcd8b686d1067a4a0769a384326e19cd35aef
Krzysiek Swol <64749798+krzysiekswol@users.noreply.github.com>
2020-10-17 12:40:23 +0200

    add js classlist

--- main.js
+++ main.js
@@ -1,137 +1,171 @@
+const offer = document.querySelector("a.offer");
+const divOne = document.querySelector('div#off1');
 
-// ================================================================= Arrow =======================================================
-const arrowUp = document.querySelector(".fa-angle-double-up");
-const arrowDown = document.querySelector (".fa-angle-double-down")
-const bonus = document.querySelector(".bonus");
+const navUs = document.querySelector("a.weare");
+const divZero = document.querySelector("div#off0");
 
-const changeArrow = () => {
+const contact = document.querySelector("a.contact");
 
-    arrowDown.classList.toggle("inactive");
-    arrowUp.classList.toggle("inactive");
-    bonus.classList.toggle("active");
-}
+navUs.addEventListener('click', ()=> {
+   divOne.classList.add("d-none");
+   divTwo.classList.add('d-none');
+})
 
+offer.addEventListener("click", ()=> {
+    divOne.classList.remove("d-none");
+    divTwo.classList.add('d-none');
+    
+})
 
-arrowDown.addEventListener('click', changeArrow);
-arrowUp.addEventListener('click', changeArrow);
+const docs = document.querySelector("a.doc");
+const divTwo = document.querySelector('div#off2');
 
-//=================================================================== Slider =======================================================
+docs.addEventListener('click', ()=>{
+    divTwo.classList.remove('d-none');
+    divOne.classList.add("d-none");
+   
+})
 
-const slideList = [
-{ img: "images/baner1.png"
-
-},
-{img: "images/baner2.png"
-
-},
-{img: "images/baner3.png"
-
-},
-{img: "images/baner4.png"
-
-},
-{img: "images/baner5.png"
-
-},
-{img: "images/baner6.png"
-
-}]
-
-const image = document.querySelector("img.one");
-const dots = [...document.querySelectorAll(".dots span")];
-// const pRight = document.querySelector(".dots p.right");
-// const pLeft = document.querySelector(".dots p.left");
-const dotOne = document.querySelector("span#dot1");
-const dotTwo = document.querySelector("span#dot2");
-const dotThree = document.querySelector("span#dot3");
-const dotFour = document.querySelector("span#dot4");
-const dotFive = document.querySelector("span#dot5");
-const dotSix = document.querySelector("span#dot6");
-
-//interfejs
-const time = 4000;
-let active = 0;
-
-//implementacja
-const changeDot = () => {
-const activeDot = dots.findIndex(dot => dot.classList.contains('act'));
-dots[activeDot].classList.remove('act');
-dots[active].classList.add('act');
-};
-const changeSlide = () => {
-    active++;
-    if (active == slideList.length) {
-        active = 0;
-    }
-    image.src = slideList[active].img
-    changeDot();
-}
+contact.addEventListener('click', ()=> {
+    divOne.classList.add("d-none");
+    divTwo.classList.add('d-none');
+} )
 
 
-// const changeReverseSlide = () => {
-//     active--;
-//     if (active < 0) {
-//         active = 5;
+
+// // ================================================================= Arrow =======================================================
+// const arrowUp = document.querySelector(".fa-angle-double-up");
+// const arrowDown = document.querySelector (".fa-angle-double-down")
+// const bonus = document.querySelector(".bonus");
+
+// const changeArrow = () => {
+
+//     arrowDown.classList.toggle("inactive");
+//     arrowUp.classList.toggle("inactive");
+//     bonus.classList.toggle("active");
+// }
+
+
+// arrowDown.addEventListener('click', changeArrow);
+// arrowUp.addEventListener('click', changeArrow);
+
+// //=================================================================== Slider =======================================================
+
+// const slideList = [
+// { img: "images/baner1.png"
+
+// },
+// {img: "images/baner2.png"
+
+// },
+// {img: "images/baner3.png"
+
+// },
+// {img: "images/baner4.png"
+
+// },
+// {img: "images/baner5.png"
+
+// },
+// {img: "images/baner6.png"
+
+// }]
+
+// const image = document.querySelector("img.one");
+// const dots = [...document.querySelectorAll(".dots span")];
+// // const pRight = document.querySelector(".dots p.right");
+// // const pLeft = document.querySelector(".dots p.left");
+// const dotOne = document.querySelector("span#dot1");
+// const dotTwo = document.querySelector("span#dot2");
+// const dotThree = document.querySelector("span#dot3");
+// const dotFour = document.querySelector("span#dot4");
+// const dotFive = document.querySelector("span#dot5");
+// const dotSix = document.querySelector("span#dot6");
+
+// //interfejs
+// const time = 4000;
+// let active = 0;
+
+// //implementacja
+// const changeDot = () => {
+// const activeDot = dots.findIndex(dot => dot.classList.contains('act'));
+// dots[activeDot].classList.remove('act');
+// dots[active].classList.add('act');
+// };
+// const changeSlide = () => {
+//     active++;
+//     if (active == slideList.length) {
+//         active = 0;
 //     }
 //     image.src = slideList[active].img
 //     changeDot();
 // }
 
-let indexInterval = setInterval(changeSlide, time);
-// pRight.addEventListener('click', changeSlide)
-// pLeft.addEventListener('click', changeReverseSlide)
-dotOne.addEventListener('click', ()=>{
-    active = 0;
-    clearInterval(indexInterval);
-    image.src = slideList[active].img
-    changeDot();
-    indexInterval = setInterval(changeSlide, time);
 
-})
-dotTwo.addEventListener('click', ()=>{
-    active = 1;
-    clearInterval(indexInterval);
-    image.src = slideList[active].img
-    changeDot();
-    indexInterval = setInterval(changeSlide, time);
+// // const changeReverseSlide = () => {
+// //     active--;
+// //     if (active < 0) {
+// //         active = 5;
+// //     }
+// //     image.src = slideList[active].img
+// //     changeDot();
+// // }
+
+// let indexInterval = setInterval(changeSlide, time);
+// // pRight.addEventListener('click', changeSlide)
+// // pLeft.addEventListener('click', changeReverseSlide)
+// dotOne.addEventListener('click', ()=>{
+//     active = 0;
+//     clearInterval(indexInterval);
+//     image.src = slideList[active].img
+//     changeDot();
+//     indexInterval = setInterval(changeSlide, time);
+
+// })
+// dotTwo.addEventListener('click', ()=>{
+//     active = 1;
+//     clearInterval(indexInterval);
+//     image.src = slideList[active].img
+//     changeDot();
+//     indexInterval = setInterval(changeSlide, time);
 
     
-})
-dotThree.addEventListener('click', ()=>{
-    active = 2;
-    clearInterval(indexInterval);
-    image.src = slideList[active].img
-    changeDot();
-    indexInterval = setInterval(changeSlide, time);
+// })
+// dotThree.addEventListener('click', ()=>{
+//     active = 2;
+//     clearInterval(indexInterval);
+//     image.src = slideList[active].img
+//     changeDot();
+//     indexInterval = setInterval(changeSlide, time);
 
 
-})
-dotFour.addEventListener('click', ()=>{
-    active = 3;
-    clearInterval(indexInterval);
-    image.src = slideList[active].img
-    changeDot();
-    indexInterval = setInterval(changeSlide, time);
+// })
+// dotFour.addEventListener('click', ()=>{
+//     active = 3;
+//     clearInterval(indexInterval);
+//     image.src = slideList[active].img
+//     changeDot();
+//     indexInterval = setInterval(changeSlide, time);
 
 
-})
-dotFive.addEventListener('click', ()=>{
-    active = 4;
-    clearInterval(indexInterval);
-    image.src = slideList[active].img
-    changeDot();
-    indexInterval = setInterval(changeSlide, time);
+// })
+// dotFive.addEventListener('click', ()=>{
+//     active = 4;
+//     clearInterval(indexInterval);
+//     image.src = slideList[active].img
+//     changeDot();
+//     indexInterval = setInterval(changeSlide, time);
 
 
-})
-dotSix.addEventListener('click', ()=>{
-    active = 5;
-    clearInterval(indexInterval);
-    image.src = slideList[active].img
-    changeDot();
-    indexInterval = setInterval(changeSlide, time);
+// })
+// dotSix.addEventListener('click', ()=>{
+//     active = 5;
+//     clearInterval(indexInterval);
+//     image.src = slideList[active].img
+//     changeDot();
+//     indexInterval = setInterval(changeSlide, time);
 
 
-})
+// })
 
 
